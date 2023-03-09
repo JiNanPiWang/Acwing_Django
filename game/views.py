11bb1bf4dbc24b427ea8184881c007109bce2a86
Django docker <purep@qq.com>
2023-03-09 11:05:29 +0800

    write my first page

--- game/views.py
+++ game/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    line1 = '<h1 style="text-align: center">看我</h1>'
+    line2 = '<h1 style="text-align: center"><img src="https://i.postimg.cc/Kz02yNBd/IMG20210516193807.jpg" width=400></h1>'
+    return HttpResponse(line1 + line2)
