6c55adf1a87c3761813137f97694c59ec46b31fa
Django docker <purep@qq.com>
2023-03-10 11:57:02 +0800

    create project structure

--- game/views.py
+++
@@ -1,7 +0,0 @@
-from django.http import HttpResponse
-
-def index(request):
-    line1 = '<h1 style="text-align: center">看我</h1>'
-    line2 = '<h1 style="text-align: center"><img src="https://i.postimg.cc/Kz02yNBd/IMG20210516193807.jpg" width=400>' \
-            '</h1>'
-    return HttpResponse(line1 + line2)
